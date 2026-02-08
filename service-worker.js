c8b67aa9ee95a18e4e72ef5dd8fca6d7396de4b6
Blood donor find <athulrajk966@gmail.com>
2026-02-08 19:20:59 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,1 @@
-const CACHE_NAME = "Blood-link-cache-v1";
-const urlsToCache = [
-  "/Blood-link/index.html",
-  "/Blood-link/manifest.json"
-];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
-});
